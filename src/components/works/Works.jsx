9dc4224435eab4acf4b37d58ab700a9c2d291d9b
bertcuyugan <subs.cuyugan@gmail.com>
2021-10-17 20:38:39 +1100

    initial commit to this project

---
+++ src/components/works/Works.jsx
@@ -0,0 +1,20 @@
+import "./works.scss";
+
+export default function Works() {
+  return (
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="items">
+            <div className="left">
+              <div className="leftContainer"></div>
+            </div>
+            <div className="right">
+              <div className="rightContainer"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
